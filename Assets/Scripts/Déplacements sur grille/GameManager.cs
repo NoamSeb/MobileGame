b58de30f9d0b568059c80e64abf8be468f79ffe1
Arthur "Malukker" Beauquesne <arthur.beauquesne@gmail.com>
2025-02-10 15:11:25 +0100

    [MOD] Added comments, sorted the files, and added game manager prefab

--- Assets/Scripts/Déplacements sur grille/GameManager.cs
+++ Assets/Scripts/Déplacements sur grille/GameManager.cs
@@ -4,7 +4,12 @@ using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
+    //L'objet GameManager est un singleton présent dans chaque scène
     public static GameManager Instance;
+
+    /// <summary>
+    /// Pour remplacer la classe Bounds native qui ne fonctionnait pas
+    /// </summary>
     public struct NotBounds
     {
         public Vector3 min { get; private set; }
@@ -24,7 +29,6 @@ public class GameManager : MonoBehaviour
             return false;
         }
     }
-
     public NotBounds PlayZone { get; private set; }
 
     private void Awake()
@@ -34,17 +38,11 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
 
+        //Récupération de l'extension maximale de la tilemap, et donc de la zone de jeu
         Tilemap playZoneMap = GameObject.FindGameObjectWithTag("Playzone").GetComponent<Tilemap>();
         Vector3 min = playZoneMap.cellBounds.min;
         Vector3 max = playZoneMap.cellBounds.max;
 
         PlayZone = new NotBounds(min, max);
-
-        SwipeMovement.OnSwipeEnd += DebugSwipe;
-    }
-
-    void DebugSwipe(SwipeMovement.SwipeDirection direction)
-    {
-        Debug.Log("aezeazeaeazaez");
     }
 }

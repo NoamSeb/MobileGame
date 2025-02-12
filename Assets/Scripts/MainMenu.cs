3225974184a207b8de771e5287b190de79a651f0
noamSeb <noam.sebahoun@gmail.com>
2025-02-12 15:21:07 +0100

    [MOD] Add interaction and save settings

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,30 +1,45 @@
+using System;
 using System.Collections;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    [Header("Audio")] 
-    [SerializeField] private AudioClip _launchSFX;
-    [SerializeField] private AudioClip _menuMusic;
-    [SerializeField] private AudioSource _audioSource;
+    [Header("Level Manager")] [SerializeField]
+    private LevelManager _levelManager;
 
-    [Header("Events")] 
-    [SerializeField] UnityEvent OpenSettingsMenu;
-    [SerializeField] UnityEvent CloseSettingsMenu;
-    
-    [Header("Level Manager")]
-    [SerializeField] private LevelManager _levelManager;
-    
-    [Header("Load screen information")]
-    [SerializeField] private GameObject _loadScreen;
+    [Header("Load screen information")] [SerializeField]
+    private GameObject _loadScreen;
+
     [SerializeField] private TextMeshProUGUI _progressValue;
+    
+    [Space]
+    
+    [Foldout("Audio")]
+    [SerializeField] private AudioClip _launchSFX;
+    [Foldout("Audio")]
+    [SerializeField] private AudioClip _menuMusic;
+    [Foldout("Audio")]
+    [SerializeField] private AudioSource _audioSource;
+    
+    [Foldout("Events")]
+    [SerializeField] UnityEvent OpenSettingsMenu;
+    [Foldout("Events")]
+    [SerializeField] UnityEvent CloseSettingsMenu;
+
+    [Foldout("Settings")]
+    [SerializeField] Slider _volume;
+    [Foldout("Settings")]
+    [SerializeField] Toggle _isHapticEnable;
 
     private void Start()
     {
         _audioSource.PlayOneShot(_menuMusic);
+        _audioSource.volume = PlayerPrefs.GetFloat("volume");
     }
 
     public void Play()
@@ -51,15 +66,35 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("GameScene");
     }
 
+    #region Settings
+
     public void OpenSettings()
     {
+        GetSettingsValue();
         OpenSettingsMenu.Invoke();
     }
 
+    private void GetSettingsValue()
+    {
+        _volume.value = PlayerPrefs.GetFloat("Volume");
+        _isHapticEnable.isOn = PlayerPrefs.GetInt("IsHapticEnabled") == 1 ? true : false;
+    }
     public void CloseSettings()
     {
         CloseSettingsMenu.Invoke();
     }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetFloat("Volume", _volume.value);
+        PlayerPrefs.SetString("IsHapticEnable", _isHapticEnable.isOn ? "true" : "false");
+        PlayerPrefs.Save();
+
+        _audioSource.volume = _volume.value;
+    }
+
+    #endregion
+
 
     private IEnumerator LoadNextLevelAsync()
     {
@@ -71,5 +106,4 @@ public class MainMenu : MonoBehaviour
             yield return null;
         }
     }
-    
 }

7200f63d3bb775199c0b12ac8a5a820032789da6
Arthur "Malukker" Beauquesne <arthur.beauquesne@gmail.com>
2025-02-12 10:44:13 +0100

    [ADD] Teleporter added, grid objects get their grid position more easily

--- Assets/Scripts/Objets de la grille/GridTeleporter.cs
+++ Assets/Scripts/Objets de la grille/GridTeleporter.cs
@@ -1,6 +1,32 @@
+using NaughtyAttributes;
+using System;
 using UnityEngine;
 
 public class GridTeleporter : GridObject
 {
-    
+    [ShowNonSerializedField] private Transform _otherTeleporterPos;
+    bool IsNoSecondTeleporter => _otherTeleporterPos != null;
+
+    [Button, ShowIf(nameof(IsNoSecondTeleporter))]
+    void CreateSecondTeleporter()
+    {
+        GridTeleporter temp = Instantiate(this.gameObject).GetComponent<GridTeleporter>();
+
+        SetOtherTeleporter(temp);
+        temp.SetOtherTeleporter(this);
+    }
+
+    void SetOtherTeleporter(GridTeleporter tp)
+    {
+        if (_otherTeleporterPos == null)
+        {
+            _otherTeleporterPos = tp.transform;
+        }
+    }
+
+    public static event Action<Vector2> OnTeleport;
+    protected override void Effect()
+    {
+        OnTeleport?.Invoke(_otherTeleporterPos.position);
+    }
 }

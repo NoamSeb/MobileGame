0e504f6c8b667a18b733b0c76bb0a92180af623c
imriiama <146062070+imriiama@users.noreply.github.com>
2025-02-25 15:39:12 +0100

    [ADD] biome manager
    à modifier!

---
+++ Assets/Scripts/BiomeManager.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class BiomeManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<BiomeStructure> Levels = new List<BiomeStructure>();
+
+    [Serializable]
+    public struct BiomeStructure
+    {
+        public int idBiome; 
+        public GameObject biome; 
+    }
+
+    private int currentBiomeIndex = 0; 
+
+    void Start()
+    {
+        currentBiomeIndex = PlayerPrefs.GetInt("CurrentBiomeIndex", 0);
+        ActivateBiome(currentBiomeIndex);
+    }
+
+    // active le biome correspondant à l'index donné et désactive les autres
+    private void ActivateBiome(int biomeIndex)
+    {
+        for (int i = 0; i < Levels.Count; i++)
+        {
+            if (Levels[i].biome != null)
+            {
+                Levels[i].biome.SetActive(i == biomeIndex);
+            }
+        }
+
+        PlayerPrefs.SetInt("CurrentBiomeIndex", biomeIndex);
+        PlayerPrefs.Save();
+    }
+
+    // passe au biome suivant (si possible)
+    public void NextBiome()
+    {
+        if (currentBiomeIndex < Levels.Count - 1)
+        {
+            currentBiomeIndex++;
+            ActivateBiome(currentBiomeIndex);
+        }
+    }
+
+    // revient au biome précédent (si possible)
+    public void PreviousBiome()
+    {
+        if (currentBiomeIndex > 0)
+        {
+            currentBiomeIndex--;
+            ActivateBiome(currentBiomeIndex);
+        }
+    }
+}

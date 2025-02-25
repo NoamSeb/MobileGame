ca867cee88d089f6f838e4c42235e653fa4b9599
noamSeb <noam.sebahoun@gmail.com>
2025-02-25 23:01:43 +0100

    :sparkles: [ADD] Google Play Service

--- Assets/Scripts/Déplacements sur grille/PlayerGridMovement.cs
+++ Assets/Scripts/Déplacements sur grille/PlayerGridMovement.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
-using Codice.CM.Client.Differences;
 using UnityEngine.Tilemaps;
 using TMPro;
 using static GridRotationLocker;

36416658c4b2b67067bef717a35c6c4164f69430
Arthur "Malukker" Beauquesne <arthur.beauquesne@gmail.com>
2025-02-25 14:40:41 +0100

    [ADD] Better Laser

---
+++ Assets/Scripts/Objets de la grille/GridLaserEmittor.cs
@@ -0,0 +1,103 @@
+using System;
+using UnityEngine;
+
+public class GridLaserEmittor : GridObject
+{
+    enum LaserState
+    {
+        Activating,
+        Activated,
+        Deactivating,
+        Deactivated
+    }
+    [SerializeField] private LaserState _state;
+    [SerializeField, Range(0.1f, 1f)] private float _killDistance;
+    [SerializeField, Range(2, 10)] private int _laserLength;
+    private bool _isActivated;
+    private Transform _playerPos;
+
+    public enum Direction
+    {
+        Up,
+        Left,
+        Down,
+        Right
+    }
+
+    [SerializeField] Direction _direction;
+    private Vector3Int _setupDirection;
+
+    private void OnValidate()
+    {
+        switch (_direction)
+        {
+            case Direction.Left:
+                transform.rotation = Quaternion.Euler(0, 0, 90);
+                _setupDirection = Vector3Int.left;
+                break;
+            case Direction.Down:
+                transform.rotation = Quaternion.Euler(0, 0, 180);
+                _setupDirection = Vector3Int.down;
+                break;
+            case Direction.Right:
+                transform.rotation = Quaternion.Euler(0, 0, -90);
+                _setupDirection = Vector3Int.right;
+                break;
+            case Direction.Up:
+                transform.rotation = Quaternion.identity;
+                _setupDirection = Vector3Int.up;
+                break;
+        }
+    }
+
+    protected override void Setup()
+    {
+        base.Setup();
+        _playerPos = GameManager.Instance.PlayerScript.transform;
+        PlayerGridMovement.OnActionExecuted += UpdateLaserState;
+
+        GameObject laserBlock = (GameObject)Resources.Load("Other Prefabs/LaserBlock");
+        Vector3Int tempGridPos = GridPosition + _setupDirection;
+        for (int i = 1; i < _laserLength; i++)
+        {
+            GameObject tempBlock = Instantiate(laserBlock, _grid.GetCellCenterWorld(tempGridPos), Quaternion.identity);
+            tempBlock.GetComponent<GridLaserBlock>().SecondSetup(_playerPos, _killDistance);
+            tempGridPos += _setupDirection;
+        }
+    }
+
+    public static event Action OnActivate;
+    void UpdateLaserState()
+    {
+        switch (_state)
+        {
+            case LaserState.Activating:
+                _state = LaserState.Activated; 
+                _isActivated = true; 
+                OnActivate?.Invoke();
+                break;
+
+            case LaserState.Activated:
+                _state = LaserState.Deactivating; 
+                break;
+
+            case LaserState.Deactivating:
+                _state = LaserState.Deactivated; 
+                _isActivated = true;
+                OnActivate?.Invoke();
+                break;
+
+            case LaserState.Deactivated:
+                _state = LaserState.Activating; 
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(_playerPos.position, transform.position) < _killDistance)
+        {
+            Oxygen.Instance.StopPlayer();
+        }
+    }
+}

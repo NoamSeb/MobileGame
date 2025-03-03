6f391b2b5fb542eaea13a18729dadee4763e0779
LilouTridon <lilou.tridon@gmail.com>
2025-03-03 17:16:25 +0100

    temp

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -57,7 +57,7 @@ public class LevelController : MonoBehaviour
     }
 
     public void LoadLevel(int levelID)
-    {
+    {   
         foreach (LevelStructure level in Levels)
         {
             level.level.SetActive(level.idLevel == levelID);
